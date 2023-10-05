15b652bfb937810184fbf02b97c4d804850f8067
philipfavourboluwatife247 <uniquefavour2002@gmail.com>
2023-10-05 14:56:39 -0700

    LOCKBOXES

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,52 +1,26 @@
 #!/usr/bin/python3
+"""
+ Defines a function that determines
+ if a box containing a list
+ of lists can be opened using keys
+ stored in the lists
+"""
 
-from collections import deque
-from typing import List
 
-
-def canUnlockAll(boxes: List[List[int]]) -> bool:
+def canUnlockAll(boxes):
     """
-    Determine if all boxes can be
-    unlocked starting from the first box.
-
-    Args:
-        boxes (List[List[int]]): A list
-        of lists, where each inner list
-        represents a box and contains keys
-        to other boxes.
-
-    Returns:
-        bool: True if all boxes can be
-        opened, False otherwise.
+    Determines if boxes can be unlocked
     """
+    position = 0
+    unlocked = {}
 
-    if not boxes:
-        return False
-
-    n = len(boxes)
-    visited = [False] * n
-    visited[0] = True
-
-    queue = deque([0])
-
-    while queue:
-        current_box = queue.popleft()
-        for key in boxes[current_box]:
-            if key < n and not visited[key]:
-                visited[key] = True
-                queue.append(key)
-
-    # Check if all boxes have been visited except the empty ones
-    return all(visited) and all(
-            visited[i] or not boxes[i] for i in range(1, n)
-    )
-
-# Example usage:
-# boxes1 = [[1], [2], [3], [4], []]
-# print(canUnlockAll(boxes1))
-#
-# boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-# print(canUnlockAll(boxes2))
-#
-# boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-# print(canUnlockAll(boxes3))
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
+    return False
